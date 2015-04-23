e719eb03a81ac38027949aafc2cd82b667f79564
Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
2015-04-23 23:52:32 +0200

    Let's see the names...

--- OAuth/oauth-github-2.js
+++ OAuth/oauth-github-2.js
@@ -58,7 +58,9 @@ function listFiles(error, contents) {
     if (error) {
 	repocontents.html("<p>Error code: " + error.error + "</p>");
     } else {
-	console.log(contents);
+	for (var i = 0, len = contents.lenght; i <= len; i++) {
+	    console.log(contents[i].name);
+	};
     };
 }
 
