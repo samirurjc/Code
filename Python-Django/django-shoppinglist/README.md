ef0c25c10939d9468b4fd2ef038873d557c68cfa
Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
2019-04-01 23:41:13 +0200

    Example of a very simple shopping list.

---
+++ Python-Django/django-shoppinglist/README.md
@@ -0,0 +1,12 @@
+# Shopping list
+
+Very simple shopping list web application.
+Uses a dictionary to store items and quantities to shop.
+Since the dictionary is not persistent, the shopping list
+is empty every time the application is reloaded.
+
+To run (with Django installed):
+
+```bash
+% ./manage.py runserver
+```
---
+++ Python-Django/django-shoppinglist/items/views.py
@@ -0,0 +1,67 @@
+from django.http import HttpResponse
+from django.middleware.csrf import get_token
+
+# Create your views here.
+
+html_template = """<!DOCTYPE html>
+<html lang="en" >
+  <head>
+    <meta charset="utf-8" />
+    <title>Shopping list</title>
+  </head>
+  <body>
+    {content}
+  </body>
+</html>
+"""
+
+item_page_template = """<p><a href="/">Full shopping list</a></p>
+<h2>Item: {name}</h2>
+<p>Quantity to shop: {quantity}</p>
+<form action="/{name}" method="POST">
+  New quantity: <input name="value" type="number" />
+  <input type="submit" value="Submit" />
+  <input type="hidden" name="csrfmiddlewaretoken" value="{token}" />
+</form>
+"""
+
+item_template = """
+<li><a href="/{name}">{name}</a>: {quantity}</li> 
+"""
+
+usual_items = ['milk', 'bread', 'oranges', 'butter', 'apples']
+
+usual_item_template = '<a href="/{name}">{name}</a> '
+
+items = {}
+
+def index(request):
+
+    list = "<ul>"
+    for name in items:
+        list += item_template.format(name=name, quantity=items[name])
+    list += "</ul>"
+
+    list += "<p>Usual items: "
+    for name in usual_items:
+        list += usual_item_template.format(name=name)
+    list += "</p>"
+
+    html = html_template.format(content="<h1>List of items to shop</h1>" + list)
+    return HttpResponse(html)
+
+def item(request, name):
+
+    if request.method == 'POST':
+        items[name] = request.POST.get('value')
+
+    if request.method == 'GET' or request.method == 'POST':
+        csrf_token = get_token(request)
+        if name in items:
+            quantity = str(items[name])
+        else:
+            quantity = '0'
+        content = item_page_template.format(name=name, quantity=quantity,
+                                            token=csrf_token)
+        html = html_template.format(content=content)
+    return HttpResponse(html)
