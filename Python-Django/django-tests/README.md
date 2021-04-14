ed21a55c781c64e8b4cfab3b9a903f08e26e7b83
Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
2021-04-14 18:37:19 +0200

    [django] Added some more tests

--- Python-Django/django-tests/README.md
+++ Python-Django/django-tests/README.md
@@ -1,3 +1,10 @@
 # Django CMS tests
 
 Extension of the simple Django CMS Forms code to illustrate how to add some tests.
+
+To learn about Django testing:
+
+* [Testing in Django](https://docs.djangoproject.com/en/3.2/topics/testing/):
+  entry point for docuentation on Django testing
+* [Writing your first Django app, part 5](https://docs.djangoproject.com/en/3.2/intro/tutorial05/):
+  tutorial on Django testing
--- Python-Django/django-tests/pages/tests.py
+++ Python-Django/django-tests/pages/tests.py
@@ -1,8 +1,14 @@
 from django.test import TestCase
+from django.test import Client
 
 from .views import Counter
 
 class CounterTests(TestCase):
+
+    def test_creation(self):
+        """Creation works"""
+        counter = Counter()
+        self.assertEqual(counter.count, 0)
 
     def test_increment(self):
         """Increment works"""
@@ -11,8 +17,41 @@ class CounterTests(TestCase):
         self.assertEqual(incremented, 1)
 
     def test_increment2(self):
-        """Increment works"""
+        """Increment works (check internal count)"""
         counter = Counter()
         counter.increment()
         incremented = counter.count
         self.assertEqual(incremented, 1)
+
+class GetTests (TestCase):
+
+    def test_root(self):
+        """Check root resource"""
+
+        c = Client()
+        response = c.get('/')
+        self.assertEqual(response.status_code, 200)
+        content = response.content.decode('utf-8')
+        self.assertIn('<h1>Django CMS</h1>', content)
+
+    def test_empty(self):
+        """Empty resource"""
+
+        c = Client()
+        response = c.get('/empty')
+        self.assertEqual(response.status_code, 404)
+        content = response.content.decode('utf-8')
+        self.assertIn('<a href="/">Main page</a>', content)
+
+    def test_create(self):
+        """Empty resource"""
+
+        item_content = "This is the content for item."
+        c = Client()
+        response = c.post('/item', {'content': item_content})
+        self.assertEqual(response.status_code, 200)
+        response = c.get('/item')
+        self.assertEqual(response.status_code, 200)
+        content = response.content.decode('utf-8')
+        self.assertIn(item_content, content)
+        self.assertIn('<form action="/item" method="post">', content)
